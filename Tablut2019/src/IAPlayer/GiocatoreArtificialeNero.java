18eafa58378efcb604425a0c4a7fc454cb6de572
s0000908933 <s0000908933@LAB2N23.LAB2>
2019-05-06 12:51:18 +0200

    Aggiunta classi per giocatore artificiale specifico

---
+++ Tablut2019/src/IAPlayer/GiocatoreArtificialeNero.java
@@ -0,0 +1,13 @@
+package IAPlayer;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+public class GiocatoreArtificialeNero {
+	
+	public static void main(String[] args) throws UnknownHostException, ClassNotFoundException, IOException {
+		String[] array = new String[]{"black"};
+		GiocatoreArtificiale.main(array);
+	}
+
+}
