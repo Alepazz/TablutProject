061e9ac159dfc6445da4bb2052a8cf79402f40d7
Piretts <Piretts@AndreaPC>
2019-05-09 13:47:24 +0200

    Sort del primo livello dell'albero

--- Tablut2019/src/IAPlayer/IntelligenzaNera.java
+++ Tablut2019/src/IAPlayer/IntelligenzaNera.java
@@ -1095,6 +1095,7 @@ public class IntelligenzaNera implements IA {
 		//ho il re sotto
 		if (a.getRowTo()<state.getBoard().length-2&&state.getPawn(a.getRowTo()+1,a.getColumnTo()).equalsPawn("K"))
 		{
+			//System.out.println("Ho il re sotto");
 			//re sul trono
 			if(state.getBox(a.getRowTo()+1, a.getColumnTo()).equals("e5"))
 			{
@@ -1324,7 +1325,7 @@ public class IntelligenzaNera implements IA {
 			Thread t = new Thread(treeGenerator);
 			t.start();
 			//this.wait(30000);
-			Thread.sleep(20000);
+			Thread.sleep(5000);
 			//System.out.println("Lancio l'interruzione");
 			treeGenerator.stopThread();
 			//t.interrupt();
@@ -1342,7 +1343,7 @@ public class IntelligenzaNera implements IA {
 			t = new Thread(heuristicValuator);
 			t.start();
 			//this.wait(25000);
-			Thread.sleep(20000);
+			Thread.sleep(5000);
 			//System.out.println("Lancio l'interruzione");
 			//t.interrupt();
 			heuristicValuator.stopThread();
